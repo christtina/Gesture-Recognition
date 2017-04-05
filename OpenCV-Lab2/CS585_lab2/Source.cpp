cec572bda56d3ff46d839925f2a4c5ccbeaf8f32
Christina Howard <christina.vhoward@gmail.com>
2017-04-05 18:05:29 -0400

    Update Source.cpp

--- OpenCV-Lab2/CS585_lab2/Source.cpp
+++ OpenCV-Lab2/CS585_lab2/Source.cpp
@@ -1,29 +1,14 @@
-/*	CS585_Lab2.cpp
-*	CS585 Image and Video Computing Fall 2014
-*	
-*	used for:
-*	CS440 Lab: Introduction to OpenCV
-*	--------------
-*	This program introduces the following concepts:
-*		a) Reading a stream of images from a webcamera, and displaying the video
-*		b) Skin color detection
-*		c) Background differencing
-*		d) Visualizing motion history
-*	--------------
-*/
-
-//opencv libraries
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
-//C++ standard libraries
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include <vector>
+
 
 using namespace cv;
 using namespace std;
-
-//function declarations
 
 /**
 Function that returns the maximum of 3 integers
@@ -64,13 +49,25 @@ Function that accumulates the frame differences for a certain number of pairs of
 */
 void myMotionEnergy(vector<Mat> mh, Mat& dst);
 
+
+/**
+Function that does some sort of detection of the three image processing
+**/
+void condefects(vector<Vec4i> convexityDefectsSet, vector<Point> mycontour, Mat &frame);
+
+
+/** Global Varialbes **/
+int thresh = 128;
+int max_thresh = 255;
+
+
+/** Main Function **/
 int main()
 {
 
 	//----------------
 	//a) Reading a stream of images from a webcamera, and displaying the video
 	//----------------
-	// For more information on reading and writing video: http://docs.opencv.org/modules/highgui/doc/reading_and_writing_images_and_video.html
 	// open the video camera no. 0
 	VideoCapture cap(0);
 
@@ -83,7 +80,7 @@ int main()
 
 	//create a window called "MyVideoFrame0"
 	namedWindow("MyVideo0", WINDOW_AUTOSIZE);
-	Mat frame0;
+	Mat frame0; //what is the Mat datatype
 
 	// read a new frame from video
 	bool bSuccess0 = cap.read(frame0);
@@ -91,16 +88,18 @@ int main()
 	//if not successful, break loop
 	if (!bSuccess0)
 	{
-		cout << "Cannot read a frame from video stream" << endl;
+		cout << "Cannot read a frame from video stream" << endl;//how are you breaking the loop
 	}
 
 	//show the frame in "MyVideo" window
 	imshow("MyVideo0", frame0);
 
-	//create a window called "MyVideo"
+	//create a window called "MyVideo", "MyVideoMH", and "Skin"
 	namedWindow("MyVideo", WINDOW_AUTOSIZE);
 	namedWindow("MyVideoMH", WINDOW_AUTOSIZE);
 	namedWindow("Skin", WINDOW_AUTOSIZE);
+	namedWindow("RockScissorPaper", WINDOW_AUTOSIZE);
+
 
 	vector<Mat> myMotionHistory;
 	Mat fMH1, fMH2, fMH3;
@@ -125,47 +124,114 @@ int main()
 		}
 
 		// destination frame
-		Mat frameDest;
-		frameDest = Mat::zeros(frame.rows, frame.cols, CV_8UC1); //Returns a zero array of same size as src mat, and of type CV_8UC1
+		Mat SkinframeDest;
+		SkinframeDest = Mat::zeros(frame.rows, frame.cols, CV_8UC1); //Returns a zero array of same size as src mat, and of type CV_8UC1
 		//----------------
 		//	b) Skin color detection
 		//----------------
-		mySkinDetect(frame, frameDest);
-		imshow("Skin", frameDest);
+		mySkinDetect(frame, SkinframeDest);
+		imshow("Skin", SkinframeDest);
 
-		//----------------
-		//	c) Background differencing
-		//----------------
+		// Convert into binary image using thresholding
+		// Documentation for threshold: http://docs.opencv.org/modules/imgproc/doc/miscellaneous_transformations.html?highlight=threshold#threshold
+		// Example of thresholding: http://docs.opencv.org/doc/tutorials/imgproc/threshold/threshold.html
+		Mat thres_output;
+		threshold(SkinframeDest, thres_output, thresh, max_thresh, 0);
 
+		vector<vector<Point>> contours;
+		vector<Vec4i> hierarchy;
+		// Find contours
+		// Documentation for finding contours: http://docs.opencv.org/modules/imgproc/doc/structural_analysis_and_shape_descriptors.html?highlight=findcontours#findcontours
+		findContours(thres_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+		cout << "The number of contours detected is: " << contours.size() << endl;
 
-		//call myFrameDifferencing function
-		myFrameDifferencing(frame0, frame, frameDest);
-		imshow("MyVideo", frameDest);
-		myMotionHistory.erase(myMotionHistory.begin());
-		myMotionHistory.push_back(frameDest);
-		Mat myMH = Mat::zeros(frame0.rows, frame0.cols, CV_8UC1);
+		//Mat frameDest = Mat::zeros(thres_output.size(), CV_8UC3);
+		// Find largest contour
+		int maxsize = 0;
+		int maxind = 0;
+		Rect boundrec;
+		for (int i = 0; i < contours.size(); i++)
+		{
+			// Documentation on contourArea: http://docs.opencv.org/modules/imgproc/doc/structural_analysis_and_shape_descriptors.html#
+			double area = contourArea(contours[i]);
+			if (area > maxsize) {
+				maxsize = area;
+				maxind = i;
+				boundrec = boundingRect(contours[i]);
+			}
+		}
 
-		//----------------
-		//  d) Visualizing motion history
-		//----------------
+		/// Find the convex hull and defect object for each contour
+		vector<vector<Point> >hull(contours.size());
+		vector<vector<Vec4i>>defects(contours.size());
+		vector<vector<int> >inthull(contours.size());
+		for (int i = 0; i < contours.size(); i++)
+		{
+			convexHull(Mat(contours[i]), hull[i], false);
+			convexHull(Mat(contours[i]), inthull[i], false);
+			if (inthull[i].size() > 3) {
+				convexityDefects(contours[i], inthull[i], defects[i]);
+			}
+		}
 
-		//call myMotionEnergy function
-		myMotionEnergy(myMotionHistory, myMH);
+		// Draw contours
+		// Documentation for drawing contours: http://docs.opencv.org/modules/imgproc/doc/structural_analysis_and_shape_descriptors.html?highlight=drawcontours#drawcontours
+		// Documentation for drawing rectangle: http://docs.opencv.org/modules/core/doc/drawing_functions.html
+		rectangle(frame, boundrec, Scalar(0, 255, 0), 1, 8, 0);
+		drawContours(frame, hull, maxind, Scalar(0, 0, 255), 2, 8, hierarchy);
+		rectangle(SkinframeDest, boundrec, Scalar(255, 255, 255), 1, 8, 0);
+		drawContours(SkinframeDest, hull, maxind, Scalar(255, 0, 255), 2, 8, hierarchy);
+		//call the condefect function which plot the 
+		condefects(defects[maxind], contours[maxind], frame);
 
+		cout << "The area of the largest contour detected is: " << contourArea(contours[maxind]) << endl;
+		cout << "-----------------------------" << endl << endl;
 
-		imshow("MyVideoMH", myMH); //show the frame in "MyVideo" window
-		frame0 = frame;
-		//wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+		/// Show in a window
+		imshow("RockScissorPaper", frame);
+		imshow("source", SkinframeDest);
 		if (waitKey(30) == 27)
 		{
 			cout << "esc key is pressed by user" << endl;
 			break;
 		}
 
+		// //----------------
+		// //	c) Background differencing
+		// //----------------
+
+
+		// //call myFrameDifferencing function
+		// myFrameDifferencing(frame0, frame, frameDest);
+		// imshow("MyVideo", frameDest);
+		// myMotionHistory.erase(myMotionHistory.begin());
+		// myMotionHistory.push_back(frameDest);
+		// Mat myMH = Mat::zeros(frame0.rows, frame0.cols, CV_8UC1);
+
+		// //----------------
+		// //  d) Visualizing motion history
+		// //----------------
+
+		// //call myMotionEnergy function
+		// myMotionEnergy(myMotionHistory, myMH);
+
+
+		// imshow("MyVideoMH", myMH); //show the frame in "MyVideo" window
+		// frame0 = frame;
+		// //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+		// if (waitKey(30) == 27)
+		// {
+		// 	cout << "esc key is pressed by user" << endl;
+		// 	break;
+		// }
+
 	}
+	waitKey(0);
 	cap.release();
 	return 0;
 }
+
+/** Function Definitions **/
 
 //Function that returns the maximum of 3 integers
 int myMax(int a, int b, int c) {
@@ -223,5 +289,43 @@ void myMotionEnergy(vector<Mat> mh, Mat& dst) {
 				dst.at<uchar>(i, j) = 255;
 			}
 		}
+	}
+}
+
+//Function that determines rock, paper, scissors
+void condefects(vector<Vec4i> Defects, vector<Point> contour, Mat &frame)
+{
+	Point2f center;
+	float x;
+	int fingers = 0;
+
+	// find the centorid of the hand 
+	minEnclosingCircle(contour, center, x);
+	circle(frame, center, 10, CV_RGB(0, 0, 255), 2, 8);
+
+	for (int i = 0; i < Defects.size(); i++) {
+
+		//extracting the start point and the depth from the Defects
+		Point ptStart(contour[Defects[i].val[0]]);
+		double depth = static_cast<double>(Defects[i].val[3]) / 256;
+		//display start points
+		circle(frame, ptStart, 5, CV_RGB(255, 0, 0), 2, 8);
+
+		//if the depth > 11 and the start point is higher than the center, count that as a finger
+		if (depth>11 && ptStart.y<center.y) {
+			circle(frame, ptStart, 4, CV_RGB(255, 0, 0), 4);
+			fingers++;
+		}
+	}
+
+	//index: if number if fingers detected: (0,1):Rock, (2,3):Scussor, (>3):Paper
+	if (fingers >1 && fingers <= 3) {
+		//putText(frame, "Scissor", Point(50, 50), 2, 2, CV_RGB(0, 255, 0), 4, 8);
+	}
+	else if (fingers <= 1) {
+		//putText(frame, "BLACK POWER", Point(50, 50), 2, 2, CV_RGB(0, 0, 0), 4, 8);
+	}
+	else if (fingers > 3) {
+		//putText(frame, "STOP", Point(50, 50), 2, 2, CV_RGB(255, 0, 0), 4, 8);
 	}
 }
